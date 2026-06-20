fab0eec1434c56a242d1ca3a287147782a0d0809
Sam Sepassi <115127751+samsepassi1@users.noreply.github.com>
2026-06-20 11:33:56 -0400

    Fix 6 runtime issues: BUCKET arg, Athena db names, SQL checks, event_id, PySpark analytics, asset metadata

--- sparkify_airflow_lakehouse/transactions/sql/songs.sql
+++ sparkify_airflow_lakehouse/transactions/sql/songs.sql
@@ -1,1 +1,7 @@
-SELECT DISTINCT song_id, title, artist_id, year, duration FROM sparkify_transactions.song_versions
+SELECT DISTINCT
+    song_id,
+    title,
+    artist_id,
+    year,
+    duration
+FROM transactions.song_versions
