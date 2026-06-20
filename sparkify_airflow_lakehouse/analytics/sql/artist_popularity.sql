cc444f5bfa420f7b037992b391b13bef825ed636
Sam Sepassi <115127751+samsepassi1@users.noreply.github.com>
2026-06-20 11:34:01 -0400

    Fix 6 runtime issues: BUCKET arg, Athena db names, SQL checks, event_id, PySpark analytics, asset metadata

--- sparkify_airflow_lakehouse/analytics/sql/artist_popularity.sql
+++ sparkify_airflow_lakehouse/analytics/sql/artist_popularity.sql
@@ -1,1 +1,8 @@
-SELECT a.artist_id,a.artist_name,count(*) plays FROM sparkify_transactions.events e JOIN sparkify_transactions.artists a ON e.artist_id=a.artist_id GROUP BY 1,2
+-- Reference SQL (analytics Glue script uses PySpark DataFrames, not SQL)
+SELECT
+    a.artist_id,
+    a.artist_name,
+    count(*) AS plays
+FROM transactions.events e
+JOIN transactions.artists a ON e.artist_id = a.artist_id
+GROUP BY 1, 2
