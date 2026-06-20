ec006c51d725942c012a2cabb7dff04bdb49416f
Sam Sepassi <115127751+samsepassi1@users.noreply.github.com>
2026-06-20 11:33:57 -0400

    Fix 6 runtime issues: BUCKET arg, Athena db names, SQL checks, event_id, PySpark analytics, asset metadata

--- sparkify_airflow_lakehouse/transactions/sql/song_versions.sql
+++ sparkify_airflow_lakehouse/transactions/sql/song_versions.sql
@@ -1,1 +1,9 @@
-SELECT song_id, title, artist_id, year, duration, current_timestamp() version_loaded_at FROM sparkify_raw.songs WHERE data_interval='{{ data_interval }}'
+SELECT
+    song_id,
+    title,
+    artist_id,
+    year,
+    duration,
+    current_timestamp() AS version_loaded_at
+FROM raw.songs
+WHERE data_interval = '{{ data_interval }}'
